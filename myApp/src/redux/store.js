b1be185d7da3e3963e82755c403cec679670201f
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-25 18:46:49 +0530

    Create store.js

---
+++ myApp/src/redux/store.js
@@ -0,0 +1,12 @@
+
+import {configureStore} from "@reduxjs/toolkit"
+import coinReducer from "./coinSlice"
+
+
+const store=configureStore({
+    reducer:{
+       coins:coinReducer
+    }
+})
+
+export default store
