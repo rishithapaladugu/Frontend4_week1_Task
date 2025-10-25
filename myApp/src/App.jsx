d278d27069935846c1b4b156ede70c613d63f1e6
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-25 18:49:07 +0530

    Create App.jsx

---
+++ myApp/src/App.jsx
@@ -0,0 +1,27 @@
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCoins } from "./redux/coinSlice";
+import SearchCoins from "./components/SearchCoins";
+import DisplayCoins from "./components/DisplayCoins";
+import SortCoins from "./components/SortCoins";
+import "./App.css"
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { data, error, isLoading } = useSelector((state) => state.coins);
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
+  return (
+    <div className="container">
+      <div className="search-sort-container">
+      <SearchCoins />
+      <SortCoins/>
+      </div>
+       <DisplayCoins/>
+    </div>
+  );
+};
+
+export default App;
